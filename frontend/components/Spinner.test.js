ecdd8a64fe7a451101857e400e48988433659082
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-07-30 13:33:35 +0200

    final submission, mvp and added tests for the spinner comp.

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,21 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import Spinner from "./Spinner";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Spinner is working properly", () => {
+  test("Spinner show up when the passed prop is true", () => {
+    render(<Spinner on={true} />);
+    screen.getByText(/please wait/i);
+  });
+
+  test("Spinner don't show up when the passed prop is false", () => {
+    render(<Spinner on={false} />);
+    const spinnerText = screen.queryByText(/please wait/i);
+    expect(spinnerText).not.toBeInTheDocument();
+    expect(spinnerText).toBeFalsy();
+  });
+});
+
